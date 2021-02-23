1dfefbafc95d1b027ca39378597b37ae18888fce
Samin-sam <saminsam362@gmail.com>
2021-02-24 03:07:29 +0600

    6_Array.php

---
+++ MID_LAB_TASK_PHP_04/6_Array.php
@@ -0,0 +1,12 @@
+<?php 
+    $array=["Samin","Aftab","Rony","Shakib","Ohi"];
+    foreach ($array as $key => $value)
+     {
+        if($value=="Samin")
+        {
+            echo "Found <b>".$value."</b> in the list.";
+        }
+    }
+
+
+?>
--- MID_LAB_TASK_PHP_05/TASK_1/TASK_C/Name2.php
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_C/Name2.php
@@ -8,7 +8,7 @@
 	<form method="POST">
 		<fieldset style="width: 300px;">
 			<legend>Name</legend>
-			Name: <input type="text" name="myname" value="<?php if(isset($_POST['myname'])){ echo $_POST['myname']; }?> }">  <br>
+			Name: <input type="text" name="myname" value="<?php if{(isset($_POST['myname'])){ echo $_POST['myname']; }?> }">  <br>
 			<hr>
 				<input type="submit" name="submit" value="Submit">
 		</fieldset>
