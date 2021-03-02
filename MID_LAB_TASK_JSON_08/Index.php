0d17fed89307983a997c4a6381f314bab0a69f62
Samin-sam <saminsam362@gmail.com>
2021-03-02 22:53:27 +0600

    index.php

---
+++ MID_LAB_TASK_JSON_08/Index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <table border="1px" style="border-collapse: collapse; width:70%; margin:auto;">
+        <tr>
+            <td colspan="2">
+                <table style="width: 100%;">
+                    <tr>
+                        <td><img src="logo1.png" alt="Logo"></td>
+                        <td style="float: right; margin-top:30px;">
+                        <a style="padding: 5px;" href="One.php">Home</a>
+                        <a style="padding: 5px;" href="login.php">Login</a>
+                        <a style="padding: 5px;" href="registration.php">Registration</a>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr style="height: 200px;">
+            <td colspan="2"><h3>Welcome to xCompany</h3></td>
+        </tr>
+        <tr>
+        <td colspan="2">
+            <p style="text-align: center;">Copyright &copy; 2021</p>
+        </td>
+        </tr>
+    </table>
+</body>
+</html>
